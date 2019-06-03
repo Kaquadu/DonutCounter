39b9d0e8d480675be9b6bd49467a95df82e30765
Kuba Kowalczykowski <kkowalczykowski@prograils.com>
2019-06-03 15:51:33 +0200

    fix5

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -9,13 +9,16 @@ use Mix.Config
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or yourself later on).
 config :donuts, DonutsWeb.Endpoint,
-  secret_key_base: "OwtVVdUvPpZVtGLvopFT5JNJE5QLfAYN5fTcysp8jA4/ThDk/9G7MOI/ZBxw6ieT"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :donuts, Donuts.Repo,
   ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  username: System.get_env("DATABASE_USERNAME"),
+  password: System.get_env("DATABASE_PASSWORD"),
+  database: "donuts_prod",
+  size: 20
+  #pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
   # adapter: Ecto.Adapters.Postgres,
   # username: "postgres",
   # password: "postgres",
