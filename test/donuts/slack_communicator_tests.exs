4c00d8bd90423f5018a958cc76a7f54001526a43
Kuba Kowalczykowski <kkowalczykowski@prograils.com>
2019-06-17 14:25:12 +0200

    Donut add fix

--- test/donuts/slack_communicator_tests.exs
+++ test/donuts/slack_communicator_tests.exs
@@ -1,5 +1,7 @@
 defmodule Donuts.SlackCommunicatorTests do
+  use Donuts.DataCase
   alias Donuts.Donuts.SlackCommunicator
+  @expiration_days Application.get_env(:donuts, :donuts_expiration_days)
 
   test "add donut via fname & lname" do
     result = SlackCommunicator.process_add_donut("Kuba", "Kowalczykowski", "USLACKBOT")
