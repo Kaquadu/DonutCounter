0409f0d7eac5c9bdea72b8f38b09e14ce86128a3
Kuba Kowalczykowski <kkow@MacBook-Pro-2.localdomain>
2019-06-17 10:12:26 +0200

    Session Controller

--- lib/donuts/Sessions/session.ex
+++ lib/donuts/Sessions/session.ex
@@ -1,5 +1,9 @@
 defmodule Donuts.Sessions.Session do
   use Donuts.Schema
+  alias Plug.Conn
+  alias Donuts.Sessions
+  alias Donuts.Accounts
+
   @salt Application.get_env(:donuts, :bcrypt_salt)
 
   schema "sessions" do
@@ -18,4 +22,20 @@ defmodule Donuts.Sessions.Session do
       |> validate_required([:token, :user_id])
   end
 
+
+  def logged_in?(conn) do
+    session_token = conn |> Conn.get_session(:token)
+    if session_token, do: Sessions.check_active(session_token)
+  end
+
+  def get_current_user_name(conn) do
+      list = logged_in?(conn)
+      if list != [] and list != nil do
+        list
+          |> List.first()
+          |> Map.get(:user_id)
+          |> Accounts.get_by_id()
+          |> Map.get(:name)
+      end
+  end
 end
