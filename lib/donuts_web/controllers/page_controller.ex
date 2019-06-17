ce4fce95487e1a28b49a5d2ac2d0cbd92f6d7738
Kuba Kowalczykowski <kkowalczykowski@prograils.com>
2019-06-17 10:12:26 +0200

    Session Controller

--- lib/donuts_web/controllers/page_controller.ex
+++ lib/donuts_web/controllers/page_controller.ex
@@ -1,10 +1,9 @@
 defmodule DonutsWeb.PageController do
   use DonutsWeb, :controller
-  @client_id Application.get_env(:donuts, :client_id)
-  @redirect_uri_auth Application.get_env(:donuts, :redirect_uri_auth)
   @redirect_uri_loggedin Application.get_env(:donuts, :redirect_uri_loggedin)
   @client_secret Application.get_env(:donuts, :client_secret)
   @expiration_days Application.get_env(:donuts, :donuts_expiration_days)
+  alias Donuts.Sessions.Session
   alias Donuts.Accounts
   alias Donuts.Helpers.Auth
   alias Donuts.Helpers.HTTPHelper
@@ -13,34 +12,6 @@ defmodule DonutsWeb.PageController do
 
   def index(conn, _params) do
     render conn, "index.html"
-  end
-
-  def sign_in(conn, _params) do
-    redirect = @redirect_uri_auth |> URI.encode()
-    link = "https://slack.com/oauth/authorize?scope=identity.basic&client_id=#{@client_id}&redirect_uri=#{redirect}"
-    render(conn, "sign-in.html", link: link)
-  end
-
-  def auth(conn, params) do
-    token_info = params |> Auth.get_code() |> Auth.get_token_info()
-
-    if Map.get(token_info, "ok") == true do
-      user_id = token_info |> Map.get("user") |> Map.get("id")
-      if Accounts.get_by_slack_id(user_id) do
-        Auth.create_session(token_info)
-        conn
-          |> put_session(:token, token_info["access_token"])
-          |> redirect(to: Routes.page_path(conn, :logged_in))
-      else
-        conn
-          |> put_flash(:info, "Sorry, it seems you are not in our user database.")
-          |> redirect(to: Routes.page_path(conn, :index))
-      end
-    else
-      conn
-        |> put_flash(:info, "Sorry, it seems we have some kind of problem with logging in.")
-        |> redirect(to: Routes.page_path(conn, :index))
-    end
   end
 
   def logged_in(conn, _params) do
@@ -60,16 +31,16 @@ defmodule DonutsWeb.PageController do
 
   def add_donut(conn, %{"donut" => %{"sender_name" => sender_name}}) do
     if Accounts.get_by_real_name(sender_name) do
-      target_name = DonutsWeb.Services.UserCheck.get_current_user_name(conn)
+      target_name = Session.get_current_user_name(conn)
       target_id = Accounts.get_by_real_name(target_name) |> Map.get(:id)
 
-      %{}
-        |> Map.put(:sender, sender_name)
-        |> Map.put(:guilty, target_name)
-        |> Map.put(:user_id, target_id)
-        |> Map.put(:expiration_date, DateTime.add(DateTime.utc_now(), @expiration_days * 24 * 60 * 60, :second))
-        |> Map.put(:delivered, false)
-        |> Donuts.Donuts.create_donut()
+      %{:sender => sender_name,
+        :guilty => target_name,
+        :user_id => target_id,
+        :expiration_date => DateTime.add(DateTime.utc_now(), @expiration_days * 24 * 60 * 60, :second),
+        :delivered => false}
+      |> Donuts.Donuts.create_donut()
+
       conn |> redirect(to: Routes.page_path(conn, :logged_in))
     else
       conn
