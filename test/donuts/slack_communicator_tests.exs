d38e4f2dcc0eda2d225bdf6e314618905915ec90
Kuba Kowalczykowski <kkow@MacBook-Pro-2.localdomain>
2019-06-17 14:19:50 +0200

    Donut add fix

--- test/donuts/slack_communicator_tests.exs
+++ test/donuts/slack_communicator_tests.exs
@@ -1,3 +1,8 @@
 defmodule Donuts.SlackCommunicatorTests do
-  
+  alias Donuts.Donuts.SlackCommunicator
+
+  test "add donut via fname & lname" do
+    result = SlackCommunicator.process_add_donut("Kuba", "Kowalczykowski", "USLACKBOT")
+    assert result = {:noreply, nil}
+  end
 end
