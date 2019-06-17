10439709ba715d059817fdf5ddbac03005d471aa
Kuba Kowalczykowski <kkow@MacBook-Pro-2.localdomain>
2019-06-17 14:29:52 +0200

    Donut add fix

--- test/donuts/slack_communicator_tests.exs
+++ test/donuts/slack_communicator_tests.exs
@@ -1,9 +1,8 @@
 defmodule Donuts.SlackCommunicatorTests do
   use Donuts.DataCase
   alias Donuts.Donuts.SlackCommunicator
-  @expiration_days Application.get_env(:donuts, :donuts_expiration_days)
 
-  test "add donut via fname & lname" do
+  test "add valid donut via fname & lname" do
     result = SlackCommunicator.process_add_donut("Kuba", "Kowalczykowski", "USLACKBOT")
     assert result = {:noreply, nil}
   end
