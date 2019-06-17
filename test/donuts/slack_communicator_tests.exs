70b84763bf1deda844faabaaf924ad7b43e87b99
Kuba Kowalczykowski <kkow@MacBook-Pro-2.localdomain>
2019-06-17 15:09:14 +0200

    Tryin to test

--- test/donuts/slack_communicator_tests.exs
+++ test/donuts/slack_communicator_tests.exs
@@ -2,8 +2,17 @@ defmodule Donuts.SlackCommunicatorTests do
   use Donuts.DataCase
   alias Donuts.Donuts.SlackCommunicator
 
-  test "add valid donut via fname & lname" do
-    result = SlackCommunicator.process_add_donut("Kuba", "Kowalczykowski", "USLACKBOT")
-    assert result = {:noreply, nil}
+  test "add invalid donut / nil nil" do
+    fname = nil
+    lname = nil
+    result = SlackCommunicator.process_add_donut(fname, lname, "USLACKBOT")
+      |> Map.get("message") |> Map.get("text")
+    expected_result = "Oops! There is no such person!"
+    assert result = expected_result
   end
+
+  # test "add invalid donut via fname & lname" do
+  #   result = SlackCommunicator.process_add_donut("Kuba", "Kowalczykowski", "USLACKBOT")
+  #   assert result = {:noreply, nil}
+  # end
 end
