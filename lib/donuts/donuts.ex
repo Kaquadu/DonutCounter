3726a88d13a66df54732d30fc1193ce4c9b49ca8
Kuba Kowalczykowski <kkow@MacBook-Pro-2.localdomain>
2019-06-17 13:39:46 +0200

    Heroku final

--- lib/donuts/donuts.ex
+++ lib/donuts/donuts.ex
@@ -20,6 +20,21 @@ defmodule Donuts.Donuts do
     |> Repo.insert
   end
 
+  def add_new_donut(sender_name) do
+    if Accounts.get_by_real_name(sender_name) do
+      target_name = Session.get_current_user_name(conn)
+      target_id = Accounts.get_by_real_name(target_name) |> Map.get(:id)
+
+      %{:sender => sender_name,
+        :guilty => target_name,
+        :user_id => target_id,
+        :expiration_date => DateTime.add(DateTime.utc_now(), @expiration_days * 24 * 60 * 60, :second),
+        :delivered => false}
+      |> create_donut()
+    end
+    :ok
+  end
+
   def delete_donut(%Donut{} = donut) do
     Repo.delete(donut)
   end
