5f9e5083bb21d70ce5113baf73d629ea1d41bd4c
Kuba Kowalczykowski <kkowalczykowski@prograils.com>
2019-06-18 15:57:42 +0200

    Formatter & tests

--- lib/donuts/accounts/user.ex
+++ lib/donuts/accounts/user.ex
@@ -2,19 +2,19 @@ defmodule Donuts.Accounts.User do
   use Donuts.Schema
 
   schema "users" do
-    field :slack_id, :string
-    field :name, :string
-    field :is_admin, :boolean
+    field(:slack_id, :string)
+    field(:name, :string)
+    field(:is_admin, :boolean)
 
-    has_many :donuts, Donuts.Donuts.Donut, on_delete: :delete_all
-    has_many :sessions, Donuts.Sessions.Session, on_delete: :delete_all
+    has_many(:donuts, Donuts.Donuts.Donut, on_delete: :delete_all)
+    has_many(:sessions, Donuts.Sessions.Session, on_delete: :delete_all)
     timestamps()
   end
 
   def changeset(user, attrs) do
     user
-      |> cast(attrs, [:slack_id, :name])
-      |> validate_required([:slack_id, :name])
-      |> unique_constraint(:slack_id)
+    |> cast(attrs, [:slack_id, :name])
+    |> validate_required([:slack_id, :name])
+    |> unique_constraint(:slack_id)
   end
 end
