8f25daf05841cba824900884b8c362abab46f2c8
Kuba Kowalczykowski <kkowalczykowski@prograils.com>
2019-06-19 15:25:03 +0200

    Context name change - Donuts - RoundPies

---
+++ config/config.secret.exs
@@ -0,0 +1,8 @@
+use Mix.Config
+
+config :donuts,
+  bcrypt_salt: "$2b$12$x0DHri2vRvSt.qrWyLJOVu",
+  verification_token: "d77fq65TdeT0hvpraviVUpnE",
+  oauth_token: "xoxp-632634181587-644044063701-658456716306-d1db62cbe564047cf324a26cad126e31",
+  client_id: "632634181587.644062506869",
+  client_secret: "8ce5c827f81a18909c247b81436f72bc"
