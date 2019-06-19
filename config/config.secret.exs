9a87a886323b0e8637419249f7a6fde9e38ef522
Mac <mac@mareks-Air.localdomain>
2019-06-19 11:19:44 +0200

    Formatter

--- config/config.secret.exs
+++ config/config.secret.exs
@@ -3,6 +3,6 @@ use Mix.Config
 config :donuts,
   bcrypt_salt: "$2b$12$x0DHri2vRvSt.qrWyLJOVu",
   verification_token: "d77fq65TdeT0hvpraviVUpnE",
-  oauth_token: "xoxp-632634181587-644044063701-671415922567-eb064d0a28d3843a74b1e13c570a43cd",
+  oauth_token: "xoxp-632634181587-644044063701-669179316132-541e9be753b678375de825f65909b04d",
   client_id: "632634181587.644062506869",
   client_secret: "8ce5c827f81a18909c247b81436f72bc"
