395fbf34e84846b3b30ef72d0b12e3f7bfe295e2
Kuba Kowalczykowski <kkowalczykowski@prograils.com>
2019-06-19 16:58:30 +0200

    SC heroku

---
+++ config/config.secret.exs
@@ -0,0 +1,8 @@
+use Mix.Config
+
+config :donuts,
+  bcrypt_salt: "$2b$12$x0DHri2vRvSt.qrWyLJOVu",
+  verification_token: "d77fq65TdeT0hvpraviVUpnE",
+  oauth_token: "xoxp-632634181587-644044063701-658587797426-e08b6930c6a025fc39bca17a93c1b025",
+  client_id: "632634181587.644062506869",
+  client_secret: "8ce5c827f81a18909c247b81436f72bc"
