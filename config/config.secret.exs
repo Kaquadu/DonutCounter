9d31d7289ed31e1d9af652fd17abd1600119e0a5
Kuba Kowalczykowski <kkowalczykowski@prograils.com>
2019-06-19 11:36:08 +0200

    cfg

--- config/config.secret.exs
+++
@@ -1,8 +0,0 @@
-use Mix.Config
-
-config :donuts,
-  bcrypt_salt: "$2b$12$x0DHri2vRvSt.qrWyLJOVu",
-  verification_token: "d77fq65TdeT0hvpraviVUpnE",
-  oauth_token: "xoxp-632634181587-644044063701-669179316132-541e9be753b678375de825f65909b04d",
-  client_id: "632634181587.644062506869",
-  client_secret: "8ce5c827f81a18909c247b81436f72bc"
