7789b38c72f5177c9990440fcb74a917cffa2cb1
Mac <mac@mareks-Air.localdomain>
2019-06-19 15:11:52 +0200

    Add via webapp fix

--- config/config.secret.exs
+++
@@ -1,8 +0,0 @@
-use Mix.Config
-
-config :donuts,
-  bcrypt_salt: "$2b$12$x0DHri2vRvSt.qrWyLJOVu",
-  verification_token: "d77fq65TdeT0hvpraviVUpnE",
-  oauth_token: "xoxp-632634181587-644044063701-669196933508-56f4c20a67b857d82ea1029ff0049679",
-  client_id: "632634181587.644062506869",
-  client_secret: "8ce5c827f81a18909c247b81436f72bc"
