9c10b14009e118ef16c5af5343eccad4bcbe847a
Mac <mac@mareks-Air.localdomain>
2019-06-19 09:44:44 +0200

    OAuth token

--- config/config.secret.exs
+++ config/config.secret.exs
@@ -3,6 +3,6 @@ use Mix.Config
 config :donuts,
   bcrypt_salt: "$2b$12$x0DHri2vRvSt.qrWyLJOVu",
   verification_token: "d77fq65TdeT0hvpraviVUpnE",
-  oauth_token: "xoxp-632634181587-644044063701-668709351696-1949390dc27f3a162a6900c98b416049",
+  oauth_token: "xoxp-632634181587-644044063701-671415922567-eb064d0a28d3843a74b1e13c570a43cd",
   client_id: "632634181587.644062506869",
   client_secret: "8ce5c827f81a18909c247b81436f72bc"
