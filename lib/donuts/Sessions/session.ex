52c10088d06b3838136d578ccf2b2e88db652f2f
Mac <mac@mareks-Air.localdomain>
2019-06-24 14:11:53 +0200

    Logged_in? fcn fix

--- lib/donuts/Sessions/session.ex
+++ lib/donuts/Sessions/session.ex
@@ -21,21 +21,5 @@ defmodule Donuts.Sessions.Session do
     |> cast(attrs, [:token, :user_id])
     |> validate_required([:token, :user_id])
   end
-
-  def logged_in?(conn) do
-    session_token = conn |> Conn.get_session(:token)
-    if session_token, do: Sessions.check_active(session_token)
-  end
-
-  def get_current_user_name(conn) do
-    list = logged_in?(conn)
-
-    if list != [] and list != nil do
-      list
-      |> List.first()
-      |> Map.get(:user_id)
-      |> Accounts.get_by_id()
-      |> Map.get(:name)
-    end
-  end
+  
 end
