9e13cbc637a9d14a21e20e7b95a3d6be8b7798dc
Kuba Kowalczykowski <kkowalczykowski@prograils.com>
2019-06-25 09:00:56 +0200

    config

--- config/config.secret.exs
+++ config/config.secret.exs
@@ -3,6 +3,6 @@ use Mix.Config
 config :donuts,
   bcrypt_salt: "$2b$12$LbX4c4RyXF7LkBxHSpLc3O",
   verification_token: "d77fq65TdeT0hvpraviVUpnE",
-  oauth_token: "xoxp-632634181587-644044063701-668019772401-c996ed23fa6444db92a75d642b83a04a",
+  oauth_token: "xoxp-632634181587-644044063701-663854544147-16d913b55aef33decb93121e010ecf87",
   client_id: "632634181587.644062506869",
   client_secret: "8ce5c827f81a18909c247b81436f72bc"
