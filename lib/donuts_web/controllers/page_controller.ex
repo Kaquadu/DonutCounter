8c5f44ab12a789ed6c469ab7082de53042f96356
Mac <mac@mareks-Air.localdomain>
2019-06-25 11:19:47 +0200

    Struct to map

--- lib/donuts_web/controllers/page_controller.ex
+++ lib/donuts_web/controllers/page_controller.ex
@@ -17,9 +17,9 @@ defmodule DonutsWeb.PageController do
 
   def user_view(conn, user) do
     user_id = Accounts.get_by_real_name(user["username"]) |> Map.get(:id)
-    delivered_donuts = RoundPies.get_delivered_donuts(user_id)
-    expired_donuts = RoundPies.get_expired_donuts(user_id)
-    active_donuts = RoundPies.get_active_donuts(user_id)
+    delivered_donuts = RoundPies.get_delivered_donuts(user_id) |> Map.from_struct()
+    expired_donuts = RoundPies.get_expired_donuts(user_id) |> Map.from_struct()
+    active_donuts = RoundPies.get_active_donuts(user_id) |> Map.from_struct()
     render(conn, "user_view.html", user: user, delivered: delivered_donuts, 
                                     expired: expired_donuts, active: active_donuts)
   end
