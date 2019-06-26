1f3db0ec514a069169c665b4e91f2622223387ec
Kuba <kkowalczykowski@prograils.com>
2019-06-26 14:28:53 +0200

    Heroku push

---
+++ config/config.secret.exs
@@ -0,0 +1,8 @@
+use Mix.Config
+
+config :donuts,
+  bcrypt_salt: "$2b$12$LbX4c4RyXF7LkBxHSpLc3O",
+  verification_token: "d77fq65TdeT0hvpraviVUpnE",
+  oauth_token: "xoxp-632634181587-644044063701-675167716260-96542d0d33193af29fdc598a1b95767d",
+  client_id: "632634181587.644062506869",
+  client_secret: "8ce5c827f81a18909c247b81436f72bc"
