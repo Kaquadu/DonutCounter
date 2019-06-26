35b4fff168375aa969aa0ebc087e9716ff0ec7ef
Kuba <kkowalczykowski@prograils.com>
2019-06-26 16:04:59 +0200

    Inform via @ about adding donut

--- lib/donuts/slack_communicator.ex
+++ lib/donuts/slack_communicator.ex
@@ -181,10 +181,9 @@ defmodule Donuts.SlackCommunicator do
   end
 
   def add_donut({nil, sender_by_sid}, target_id) do
-    guilty = Accounts.get_by_slack_id(target_id) |> Map.get(:name)
     %{
       sender: sender_by_sid.name,
-      guilty: guilty,
+      guilty: Accounts.get_by_slack_id(target_id) |> Map.get(:name),
       user_id: Accounts.get_by_slack_id(target_id) |> Map.get(:id),
       expiration_date:
         DateTime.add(DateTime.utc_now(), @expiration_days * 24 * 60 * 60, :second),
@@ -192,15 +191,14 @@ defmodule Donuts.SlackCommunicator do
     }
     |> RoundPies.create_donut()
 
-    message = "Successfully added donut debt! #{guilty} owes us some donuts!" |> URI.encode()
+    message = "Successfully added donut debt! <@#{target_id}> owes us some donuts!" |> URI.encode()
     send_message_to_channel(@donuts_channel, message)
   end
 
   def add_donut({sender_by_rn, nil}, target_id) do
-    guilty = Accounts.get_by_slack_id(target_id) |> Map.get(:name)
     %{
       sender: sender_by_rn.name,
-      guilty: guilty,
+      guilty: Accounts.get_by_slack_id(target_id) |> Map.get(:name),
       user_id: Accounts.get_by_slack_id(target_id) |> Map.get(:id),
       expiration_date:
         DateTime.add(DateTime.utc_now(), @expiration_days * 24 * 60 * 60, :second),
@@ -208,7 +206,7 @@ defmodule Donuts.SlackCommunicator do
     }
     |> RoundPies.create_donut()
 
-    message = "Successfully added donut debt! #{guilty} owes us some donuts!" |> URI.encode()
+    message = "Successfully added donut debt! <@#{targer_id}> owes us some donuts!" |> URI.encode()
     send_message_to_channel(@donuts_channel, message)
   end
 
