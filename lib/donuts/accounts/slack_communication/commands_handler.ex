b2b8cf9c2daac60f7768bd8167a84f3ad76b7d75
Kuba <kkowalczykowski@prograils.com>
2019-06-28 12:00:11 +0200

    Help command typo

--- lib/donuts/accounts/slack_communication/commands_handler.ex
+++ lib/donuts/accounts/slack_communication/commands_handler.ex
@@ -48,14 +48,14 @@ defmodule Donuts.Slack.CommandsHandler do
     def process_slack_command("/donuts", ["help" | params], from_id, channel_id)
     when params == [] do
       message =
-      ":doughnut: *Welcome to donut counter!* :doughnut \n
+      ":doughnut: *Welcome to donut counter!* :doughnut: \n
       List of commands: \n
       _/donuts @your_name_ - add donut debt to the currently logged person. Remember to sign yourself with @your_name - we all would like to know to whom we owe delicious donuts :wink: \n
       _/donuts release @name_of_guilty_ - releases the odlest debt of @name_of_guilty marking it as delivered \n
       _/donuts remove @name_of_guilty_ - removes the newest debt of of @name_of_guildy and removes it from statistics, used in case of mistakes \n
       _/donuts help_ - displays list of commands \n
       _/donuts list_ - display list of currently active donuts \n \n
-      At the https://sleepy-taiga-28784.herokuapp.com you can sign in and check statistic. Good luck & don't forget to secure yourself :wink:" |> URI.encode
+      At the https://sleepy-taiga-28784.herokuapp.com you can sign in and check statistic. Good luck & don't forget to secure yourself :wink:" |> IO.inspect |> URI.encode
       {:info, "donuts", from_id, message, channel_id} |> Operations.message()
     end
 
