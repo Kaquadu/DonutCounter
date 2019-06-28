4e407f5b1bb0b8dacf7a49b4a28bda6d5c735ece
Kuba <kkowalczykowski@prograils.com>
2019-06-28 12:47:58 +0200

    Help message format

--- lib/donuts/accounts/slack_communication/commands_handler.ex
+++ lib/donuts/accounts/slack_communication/commands_handler.ex
@@ -52,7 +52,7 @@ defmodule Donuts.Slack.CommandsHandler do
       List of commands:
       _/donuts @your-name_ - add donut debt to the currently logged person. Remember to sign yourself with @your-name - we all would like to know to whom we owe delicious donuts :wink:
       _/donuts release @name-of-guilty_ - releases the odlest debt of @name-of-guilty marking it as delivered
-      _/donuts remove @name-of-guilty_ - removes the newest debt of of @name_of_guildy and removes it from statistics, used in case of mistakes
+      _/donuts remove @name-of-guilty_ - removes the newest debt of of @name-of-guildy and removes it from statistics, used in case of mistakes
       _/donuts help_ - displays list of commands
       _/donuts list_ - display list of currently active donuts \n
       At the <https://sleepy-taiga-28784.herokuapp.com> you can sign in and check statistic. Good luck & don't forget to secure yourself :wink:"
