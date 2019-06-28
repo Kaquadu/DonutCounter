0a88a37f3c592fbaad76b8808de01c5430abdccb
Kuba <kkowalczykowski@prograils.com>
2019-06-28 12:46:32 +0200

    Help message formating

--- lib/donuts/accounts/slack_communication/commands_handler.ex
+++ lib/donuts/accounts/slack_communication/commands_handler.ex
@@ -49,16 +49,14 @@ defmodule Donuts.Slack.CommandsHandler do
     when params == [] do
       message =
       ":doughnut: *Welcome to donut counter!* :doughnut: \n
-      List of commands: \n
-      _/donuts @your_name_ - add donut debt to the currently logged person. Remember to sign yourself with @your_name - we all would like to know to whom we owe delicious donuts :wink: \n
-      _/donuts release @name_of_guilty_ - releases the odlest debt of @name_of_guilty marking it as delivered \n
-      _/donuts remove @name_of_guilty_ - removes the newest debt of of @name_of_guildy and removes it from statistics, used in case of mistakes \n
-      _/donuts help_ - displays list of commands \n
-      _/donuts list_ - display list of currently active donuts \n \n
+      List of commands:
+      _/donuts @your-name_ - add donut debt to the currently logged person. Remember to sign yourself with @your-name - we all would like to know to whom we owe delicious donuts :wink:
+      _/donuts release @name-of-guilty_ - releases the odlest debt of @name-of-guilty marking it as delivered
+      _/donuts remove @name-of-guilty_ - removes the newest debt of of @name_of_guildy and removes it from statistics, used in case of mistakes
+      _/donuts help_ - displays list of commands
+      _/donuts list_ - display list of currently active donuts \n
       At the <https://sleepy-taiga-28784.herokuapp.com> you can sign in and check statistic. Good luck & don't forget to secure yourself :wink:"
       |> URI.encode()
-
-      message = "nie ogarniam" |> URI.encode()
       {:info, "donuts", from_id, message, channel_id} |> Operations.message()
     end
 
