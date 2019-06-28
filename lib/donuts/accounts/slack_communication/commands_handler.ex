631a354cc4abc3b075665f864a875b40e77e97b4
Kuba <kkowalczykowski@prograils.com>
2019-06-28 11:51:28 +0200

    Help command typo fix

--- lib/donuts/accounts/slack_communication/commands_handler.ex
+++ lib/donuts/accounts/slack_communication/commands_handler.ex
@@ -50,7 +50,7 @@ defmodule Donuts.Slack.CommandsHandler do
       message =
       ":doughnut: *Welcome to donut counter!* :doughnut \n
       List of commands: \n
-      _/donuts @your_name_ - add donut debt to the currently logged person. Remember to sign yourself with @your_name - we all would like know to whom we owe delicious donuts :wink: \n
+      _/donuts @your_name_ - add donut debt to the currently logged person. Remember to sign yourself with @your_name - we all would like to know to whom we owe delicious donuts :wink: \n
       _/donuts release @name_of_guilty_ - releases the odlest debt of @name_of_guilty marking it as delivered \n
       _/donuts remove @name_of_guilty_ - removes the newest debt of of @name_of_guildy and removes it from statistics, used in case of mistakes \n
       _/donuts help_ - displays list of commands \n
