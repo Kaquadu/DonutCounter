ad59ef40d48de3579da7169048a2618fbbb05bab
Kuba <kkowalczykowski@prograils.com>
2019-07-01 16:17:08 +0200

    Event handling pattern matching

--- lib/donuts/accounts/slack_communication/event_handler.ex
+++ lib/donuts/accounts/slack_communication/event_handler.ex
@@ -1,7 +1,7 @@
 defmodule Slack.EventHandler do
 
-    def handle_slack_event(event) when event["type"] == "team_join" do
-        user = event["user"]
+    def handle_slack_event(%{"type" => "team_join"} = event) do
+        usr_raw = event["user"]
         %{
             "slack_id" => usr_raw["id"],
             "name" => usr_raw["profile"]["real_name"],
